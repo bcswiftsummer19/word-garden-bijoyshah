5942b71c35e80be819f56eab0cdf194ebae8bced
Bijoy Shah <bijoy.00@gmail.com>
2019-06-09 23:18:39 +0100

    3.2

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -1,18 +1,41 @@
-//
 //  ViewController.swift
 //  Word Garden
 //
 //  Created by Bijoy on 6/9/19.
 //  Copyright © 2019 Bijoy Shah. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userGuessLabel: UILabel!
+    @IBOutlet weak var guessedLetterField: UITextField!
+    @IBOutlet weak var guessLetterButton: UIButton!
+    @IBOutlet weak var guessCountLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var flowerImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    }
+    
+    func updateUIAfterGuess(){
+        guessedLetterField.resignFirstResponder()
+        guessedLetterField.text = ""
+    }
+    
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+    }
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        updateUIAfterGuess()
+    }
+    
+    @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+        updateUIAfterGuess()
+    }
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
     }
     
 }
